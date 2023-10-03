5bed2e3ba5b964f806bb0e7d7f83d8ab3dee6a81
zaki azizi <zakiazizi1841992@gmail.com>
2023-10-03 23:15:08 +0200

    trying to solve lint error

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,11 @@
 import { getProfile } from "@/sanity/sanity.query";
 import type { ProfileType } from "@/types";
-import HeroSvg from "./icons/HeroSvg";
 import Job from "./components/Job";
 import hero from "./icons/hero-transparent.png"
 import Image from "next/image";
 
 export default async function Home() {
-  const profile: ProfileType = await getProfile();
+  const profile: ProfileType[] = await getProfile();
   return (
     <main className="max-w-7xl mx-auto lg:px-16 px-6">
       <section className="flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between gap-x-12 lg:mt-32 mt-20 mb-16">
