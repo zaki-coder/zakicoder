e4e13a48043541754e88a0f51d9fc430b3d79f8c
zaki azizi <zakiazizi1841992@gmail.com>
2023-10-04 11:15:58 +0200

    fix sanity type

--- app/(site)/components/global/Navbar.tsx
+++ app/(site)/components/global/Navbar.tsx
@@ -6,15 +6,13 @@ export default function Navbar() {
   return (
     <header className="py-4 md:px-16 px-6 border-b border-zinc-800 z-30 md:mb-28 mb-20">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
-        <div className="flex">
-          <Link href="/">
+          <Link href="/" className="flex">
             <Image src={Logo} width={40} height={40} alt="logo" />
+            <div className="border-l border-zinc-500 pl-2">
+              <p className="-mb-2">Zaki Coder</p>
+              <small className="text-xs text-gray-600">Coding dreams into reality</small>
+            </div>
           </Link>
-          <div className="border-l border-zinc-500 pl-2">
-            <p className="-mb-2">Zaki Coder</p>
-            <small className="text-xs text-gray-600">Coding dreams into reality</small>
-          </div>
-        </div>
         <nav>
           <ul className="flex items-center gap-x-8">
             <li>
--- sanity.config.ts
+++ sanity.config.ts
@@ -6,7 +6,8 @@ import {schemaTypes} from './schemas'
 export default defineConfig({
   name: 'sanity-nextjs-website',
   title: 'Sanity Next.js Site',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+
   dataset: 'production',
   basePath: '/studio',
   plugins: [deskTool()],
